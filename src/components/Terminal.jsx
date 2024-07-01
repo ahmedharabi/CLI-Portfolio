6582cef98beab317721ccab653845802cbcfd803
Ahmed Harabi <harabiahmed88@gmail.com>
2024-07-01 19:31:00 +0100

    finished building the terminal and fixed some bugs

--- src/components/Terminal.jsx
+++ src/components/Terminal.jsx
@@ -1,9 +1,20 @@
+import {useState} from "react";
+
 export default function Terminal(){
-    let commandStack=[];
+    const [commandStack,updateCommandStack]=useState([]);
+    function handleEnter(event){
+        if(event.key==="Enter" ){
+            updateCommandStack([...commandStack,"AhmedHarabi@portfolio:~$ "+ event.target.value]);
+            event.target.value=""
+
+        }
+    }
+
     return(
         <div className="window">
             <ul>
-                <li>AhmedHarabi@portfolio:~$ <input type="text" /></li>
+                {commandStack.map((command)=><li>{command}</li>)}
+                <li>AhmedHarabi@portfolio:~$ <input onKeyDown={handleEnter} type="text"/> </li>
             </ul>
         </div>
     )
